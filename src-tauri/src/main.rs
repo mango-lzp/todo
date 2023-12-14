6fdba6ea6822dd774edf35d0493a99a48f5008e2
lizhi <lizhi.lzp@antgroup.com>
2023-12-14 22:36:01 +0800

    chore: init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,13 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+   format!("Hello, {}!", name)
+}
+
+fn main() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
